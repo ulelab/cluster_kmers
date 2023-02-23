de507087ce9ce739e51b327015878318b49d9af8
Klara Kuret <kuretklara@gmail.com>
2023-02-23 10:30:56 +0100

    edit project

---
+++ cluster_kmers/__main__.py
@@ -0,0 +1,5 @@
+from cluster_kmers.run import main
+
+# %%
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+requirements = [
+  'textdistance',
+  'seaborn',
+  'scipy',
+  'matplotlib',
+  'pandas',
+  'scikit-learn',
+  'numpy',
+  'scikit-bio',
+  'logomaker',
+  'seqlogo'
+]
+
+setup(name='cluster_kmers',
+      version='0',
+      description='cluster_kmers',
+      url='http://github.com/ulelab/cluster_kmers',
+      author='Klara Kuret',
+      author_email='klara.kuret@ki.si',
+      license='MIT',
+      install_requires=requirements,
+      packages=['cluster_kmers'],
+      include_package_data=True,
+      python_requires=">=3.6",
+      entry_points={"console_scripts": ["cluster_kmers=cluster_kmers.run:main",],},
+      zip_safe=False)
